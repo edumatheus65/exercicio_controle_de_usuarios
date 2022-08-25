b45410a0f97fadebc8e1ba18d60956e6bf4ed21b
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-25 11:42:01 -0300

    Criado a arq conexao.php para conexao com o Banco de Dados

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+
+$db = "daotest";
+$host = "localhost";
+$user = "Eduardo";
+$pass = "";
+
+$conn = new PDO("mysql:dbname=$db;host=$host", $user, $pass)
---
+++ index.php
@@ -0,0 +1,9 @@
+<table border="0" width="100%">
+
+    <tr>
+        <th>Nome</th>
+        <th>E-mail</th>
+        <th>Ações</th>
+    </tr>
+
+</table>
