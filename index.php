1197bef50c1c5afd7f43908fd1515bd6608fd4d9
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-29 09:03:49 -0300

    Refatorando codigo

--- index.php
+++ index.php
@@ -1,12 +1,39 @@
 <?php
 
-    require 'conexao.php';
-
-    
+    require 'conexao.php';   
    
 
 ?>
 
-<a href="creat.php">Adicionar Usuario</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicise controle de usuarios</title>
+</head>
+<body>
 
-<?php 
+    <a href="creat.php">Adicionar Usuario</a>
+
+    <?php if(rowCount($usuarios) > 0): ?>
+
+        <table class="table" id="contacts-table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">email</th>                
+                </tr>
+            </thead>
+        </table>
+        <tbody>
+            <?php foreach($usuarios as $usuario):  ?>
+                <tr>
+                    <td scope="row" class="col-id"><?= $usuario['id']  ?></td>
+                </tr>
+        </tbody>
+    
+</body>
+</html>
