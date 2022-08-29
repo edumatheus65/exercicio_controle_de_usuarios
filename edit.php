506a8f3734b54abc0046692c44e36d8f0026cb9a
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-29 11:59:52 -0300

    Method Update funcionando com sucesso

--- edit.php
+++ edit.php
@@ -3,25 +3,29 @@
 require 'conexao.php';
 
 
-
+//Primeiro defino meu id como 0 
 $id = 0;
 
-if(isset($_GET['id']) && empty($_GET['id'] ==  false)) {
+//Depois faço a verificação pra pegar o id verdadeiro
+if(isset($_GET['id']) && empty($_GET['id'] == false)) {
     $id = ($_GET['id']);
 
 }
+
+//Depois verifico se o usuário enviou alguma informação
+//Caso o usuário não tenha enviado nenhuma informação ele passa direto para o select
 
 if(isset($_POST['nome']) && empty($_POST['nome']) == false) {
     $nome = ($_POST['nome']);
     $email = ($_POST['email']);
 
+
+    //Depois de feitas algumas alterações, nós vamos executar o UPDATE
     $stmt = "UPDATE usuarios SET nome = '$nome', email = '$email' WHERE id = '$id'";
     $conn->query($stmt);
 
     header("Location: index.php");
 }
-
-
 
 
     $stmt = "SELECT * FROM usuarios Where id = '$id'";
@@ -36,11 +40,11 @@ if(isset($_POST['nome']) && empty($_POST['nome']) == false) {
 
     }
 
- 
-
 ?>
 
-<form action="edit.php" method="POST">
+
+
+<form action="" method="POST">
 
     Nome:<br/>
     <input type="text" name="nome" value="<?php echo $dado['nome']; ?>" /><br/><br/>
@@ -48,8 +52,8 @@ if(isset($_POST['nome']) && empty($_POST['nome']) == false) {
     Email:<br/>
     <input type="text" name="email" value="<?php echo $dado['email']; ?>" /><br/><br/>
 
-    Senha:<br/>
-    <input type="password" name="pass" value="" /><br/><br/>
+    <!-- Senha:<br/>
+    <input type="password" name="pass" value="" /><br/><br/> -->
 
     <input type="submit" value="Atualizar" value="" /><br/><br/>
 
