0425ac707ffcf1e78108b35ea4e7548845d1f377
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-29 08:11:51 -0300

    foi deletado a tabela que estava no arq index, para sser criada de uma outra forma usando html e bootstrap

---
+++ edit.php
@@ -0,0 +1,50 @@
+<?php
+
+require 'conexao.php';
+
+
+
+
+
+if(isset($_GET['id']) && empty($_GET['id'])) {
+
+    $id = addcslashes($_GET['id']);
+
+    $stmt = "SELECT * FROM usuarios Where id = '$id'";
+    $stmt = $conn->query($stmt);
+
+    if($stmt->rowCount() > 0) {
+
+        $dado = $stmt->fetch();
+    } else {
+
+        header("Location: index.php");
+
+    }
+
+
+
+
+} else {
+
+    header("Location: index.php");
+
+
+}
+
+?>
+
+<form action="" method="GET">
+
+    Nome:<br/>
+    <input type="text" name="nome" value="<? echo $dado['nome']; ?>" /><br/><br/>
+
+    Email:<br/>
+    <input type="text" name="email" value="<? echo $dado['email']; ?>" /><br/><br/>
+
+    Senha:<br/>
+    <input type="password" name="pass" value="" /><br/><br/>
+
+    <input type="submit" value="Atualizar" value="" /><br/><br/>
+
+</form>
